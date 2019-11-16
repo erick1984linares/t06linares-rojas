4dce1377e3578fce7f8325ae232f80920942de64
ERICK <ylinares@unprg.edu.pe>
2019-11-16 16:57:15 -0500

    se agrego la condicional multiple 8

---
+++ linares/condicionalesmultiples/condicionalmultiple8.py
@@ -0,0 +1,35 @@
+import os
+nombre, samsung, iphone, LG=",", 0, 0, 0
+
+#imput
+nombre=os.sys.argv[1]
+samsung=int(os.sys.argv[2])
+iphone=int(os.sys.argv[3])
+LG=int(os.sys.argv[4])
+
+#procesing
+total=(samsung+iphone+LG)
+
+#verificador
+comprador_potencial=(total>=5000)
+
+#output
+print("##############################")
+print("#distribuidor de celulares")
+print("#variables:    cantidad:")
+print("nombre:",    nombre)
+print("#samsung:",  samsung)
+print("#iphone:",   iphone)
+print("#LG:",       LG)
+print("total:",     total)
+print("##############################")
+
+#condicion simple
+#si el el total supera 5000 regalar una tarjeta para google play
+if ( comprador_potencial== True ):
+    print("se ha ganado tres tarjeta para google play")
+if ( comprador_potencial>=4900 and comprador_potencial<4999 ):
+    print("se ha ganado una tarjeta para google play")
+if ( comprador_potencial<4899):
+    print("gane tarhetas para google play por compras superiores a 5000 soles")
+#fin_if
