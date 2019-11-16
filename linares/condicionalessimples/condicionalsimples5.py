f8c7a8083d09950569f87e3b20772b515ae1914c
erick1984linares <56937073+erick1984linares@users.noreply.github.com>
2019-11-16 16:59:46 -0500

    se agrego la condicional simple 5

---
+++ linares/condicionalessimples/condicionalsimples5.py
@@ -0,0 +1,31 @@
+import os
+nombre, precio1, precio2, precio3=",", 0, 0 ,0
+
+#imput
+nombre=os.sys.argv[1]
+precio1=int(os.sys.argv[2])
+precio2=int(os.sys.argv[3])
+precio3=int(os.sys.argv[4])
+
+#procesing
+total=(precio1+precio2+precio3)
+
+#verificador
+comprobando=(total>=50)
+
+#output
+print("##############################")
+print("#negocios linares")
+print("#variables:    cantidad:")
+print("nombre:",    nombre)
+print("#precio1:",  precio1)
+print("#precio2:",  precio2)
+print("#precio3:",  precio3)
+print("total:",     total)
+print("##############################")
+
+#condicion simple
+#si supera los 50 soles regalar un plato
+if ( comprobando == True ):
+    print("se ha ganado un plato")
+#fin_if
