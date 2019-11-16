66355ed428a54339548b300d8ab9e4fa1ab12044
erick1984linares <56937073+erick1984linares@users.noreply.github.com>
2019-11-16 16:58:44 -0500

    se agrego la condicional simple 2

---
+++ linares/condicionalessimples/condicionalsimples2.py
@@ -0,0 +1,29 @@
+import os
+precio_de_polos, precio_de_pantalones, precio_de_chompas=0, 0 ,0
+
+#imput
+precio_de_polos=int(os.sys.argv[1])
+precio_de_pantalones=int(os.sys.argv[2])
+precio_de_chompas=int(os.sys.argv[3])
+
+#procesing
+total=(precio_de_polos+precio_de_polos+precio_de_chompas)
+
+#verificador
+comprobando=(total>=100)
+
+#output
+print("##############################")
+print("#RIPLEY")
+print("#variables:                cantidad:")
+print("precio de polos:",         precio_de_polos)
+print("#precio de pantalones:",   precio_de_pantalones)
+print("#precio de chompas:",      precio_de_chompas)
+print("#total:",                  total)
+print("##############################")
+
+#condicion simple
+#si el total supera los 100 soles, regalar un descuento adicional
+if ( comprobando == True ):
+    print("usted se gano un descuento adicional")
+#fin_if
