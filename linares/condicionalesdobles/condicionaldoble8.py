b7e2905a981d352a1a435de9756c733bb33b8342
erick1984linares <56937073+erick1984linares@users.noreply.github.com>
2019-11-16 16:51:25 -0500

    se agrego el condicional doble 8

---
+++ linares/condicionalesdobles/condicionaldoble8.py
@@ -0,0 +1,33 @@
+import os
+nombre, samsung, iphone, LG=",", 0, 0, 0
+
+#imput
+nombre=os.sys.argv[1]
+samsung=int(os.sys.argv[2])
+iphone=int(os.sys.argv[3])
+LG=int(os.sys.argv[4])
+
+#procesing
+total=(samsung+iphone+LG)
+
+#verificador
+comprador_potencial=(total>=5000)
+
+#output
+print("##############################")
+print("#distribuidor de celulares")
+print("#variables:    cantidad:")
+print("nombre:",    nombre)
+print("#samsung:",  samsung)
+print("#iphone:",   iphone)
+print("#LG:",       LG)
+print("total:",     total)
+print("##############################")
+
+#condicion simple
+#si el el total supera 5000 regalar una tarjeta para google play
+if ( comprador_potencial== True ):
+    print("se ha ganado una tarjeta para google play")
+else:
+    print("gane tarjetas gratis para google play por compras superiores a 5000 soles")
+#fin_if
