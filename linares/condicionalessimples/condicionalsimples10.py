2335bed5596b08fe0832b8c6e85fa54a6e807007
erick1984linares <56937073+erick1984linares@users.noreply.github.com>
2019-11-16 17:01:22 -0500

    se agrego la condicional simple 10

---
+++ linares/condicionalessimples/condicionalsimples10.py
@@ -0,0 +1,31 @@
+import os
+nombre, precio_de_las_pastillas, precio_de_los_jarabes, precio_del_shampoo=",", 0, 0, 0
+
+#imput
+nombre=os.sys.argv[1]
+precio_de_las_pastillas=int(os.sys.argv[2])
+precio_de_los_jarabes=int(os.sys.argv[3])
+precio_del_shampoo=int(os.sys.argv[4])
+
+#procesing
+total=(precio_de_las_pastillas+precio_de_los_jarabes+precio_del_shampoo)
+
+#verificador
+descuento_los_martes=(total>=20)
+
+#output
+print("##############################")
+print("#MIFARMA")
+print("#variables:                cantidad:")
+print("#nombre:",    nombre)
+print("#precio de las pastillas:", precio_de_las_pastillas)
+print("#precio del jarabe:",       precio_de_los_jarabes)
+print("#precio de el shampoo:",    precio_del_shampoo)
+print("total:",                    total)
+print("##############################")
+
+#condicion simple
+#si el total supera los 20 soles dar puntos bonus
+if ( descuento_los_martes == True ):
+    print("se gano puntos banus para su siguiente compra")
+#fin_if
