c243ea272dbc333a1ea0fbd8bd4bae761a9620c7
erick1984linares <56937073+erick1984linares@users.noreply.github.com>
2019-11-16 18:21:46 -0500

    se agregaron los 180 validadores a "LOTE.BAT"

---
+++ lote.bat
@@ -0,0 +1,209 @@
+python linares/condicionalessimples/condicionalsimples1.py 5 2 3
+python linares/condicionalessimples/condicionalsimples1.py 1 4 2
+python linares/condicionalessimples/condicionalsimples1.py 6 2 2
+python linares/condicionalessimples/condicionalsimples2.py 40 30 50
+python linares/condicionalessimples/condicionalsimples2.py 30 30 20
+python linares/condicionalessimples/condicionalsimples2.py 60 55 44
+python linares/condicionalessimples/condicionalsimples3.py "ingrid" 60 90 50
+python linares/condicionalessimples/condicionalsimples3.py "yasseni" 50 30 40
+python linares/condicionalessimples/condicionalsimples3.py "vanessa" 20 25 125
+python linares/condicionalessimples/condicionalsimples4.py "erick" 200 225 300
+python linares/condicionalessimples/condicionalsimples4.py "noemi" 100 225 150
+python linares/condicionalessimples/condicionalsimples4.py "erick" 400 25 100
+python linares/condicionalessimples/condicionalsimples5.py "ana" 35 15 5
+python linares/condicionalessimples/condicionalsimples5.py "pedro" 40 2 3
+python linares/condicionalessimples/condicionalsimples5.py "fabricio" 20 4 21
+python linares/condicionalessimples/condicionalsimples6.py "pepe" 16 18 20
+python linares/condicionalessimples/condicionalsimples6.py "carlos" 15 19 20
+python linares/condicionalessimples/condicionalsimples6.py "pedro" 15 16 17
+python linares/condicionalessimples/condicionalsimples7.py "neymar" 800 900 1000
+python linares/condicionalessimples/condicionalsimples7.py "nestor" 200 950 1250
+python linares/condicionalessimples/condicionalsimples7.py "alan" 500 400 1500
+python linares/condicionalessimples/condicionalsimples8.py "magaly" 2050 1500 3000
+python linares/condicionalessimples/condicionalsimples8.py "erick" 1500 1300 500
+python linares/condicionalessimples/condicionalsimples8.py "marcial" 1650 1450 1890
+python linares/condicionalessimples/condicionalsimples9.py "juana" 50 50 30
+python linares/condicionalessimples/condicionalsimples9.py "pablo" 30 20 40
+python linares/condicionalessimples/condicionalsimples9.py "neo" 40 60 35
+python linares/condicionalessimples/condicionalsimples10.py "david" 12 5 10
+python linares/condicionalessimples/condicionalsimples10.py "cesar" 15 6 2
+python linares/condicionalessimples/condicionalsimples10.py "carla" 5 4 9
+python linares/condicionalesdobles/condicionaldoble1.py 5 3 2
+python linares/condicionalesdobles/condicionaldoble1.py 6 4 1
+python linares/condicionalesdobles/condicionaldoble1.py 1 10 4
+python linares/condicionalesdobles/condicionaldoble2.py 40 60 50
+python linares/condicionalesdobles/condicionaldoble2.py 20 45 75
+python linares/condicionalesdobles/condicionaldoble2.py 35 45 10
+python linares/condicionalesdobles/condicionaldoble3.py "rosa" 60 90 100
+python linares/condicionalesdobles/condicionaldoble3.py "pedro" 40 30 60
+python linares/condicionalesdobles/condicionaldoble3.py "ana"45 45 85
+python linares/condicionalesdobles/condicionaldoble4.py "noemi" 300 250 100
+python linares/condicionalesdobles/condicionaldoble4.py "pepe" 150 150 50
+python linares/condicionalesdobles/condicionaldoble4.py "rubi" 210 165 400
+python linares/condicionalesdobles/condicionaldoble5.py "karol" 25 15 20
+python linares/condicionalesdobles/condicionaldoble5.py "katy" 21 5 16
+python linares/condicionalesdobles/condicionaldoble5.py "paty" 45 21 30
+python linares/condicionalesdobles/condicionaldoble6.py "jhordan" 19 18 17
+python linares/condicionalesdobles/condicionaldoble6.py "erick2 17 16 15
+python linares/condicionalesdobles/condicionaldoble6.py "julia" 16 20 20
+python linares/condicionalesdobles/condicionaldoble7.py "josefa" 800 950 400
+python linares/condicionalesdobles/condicionaldoble7.py "cristian" 1250 350 200
+python linares/condicionalesdobles/condicionaldoble7.py "itala" 1100 580 460
+python linares/condicionalesdobles/condicionaldoble8.py "fernando" 1600 2500 2350
+python linares/condicionalesdobles/condicionaldoble8.py "piero" 2150 3000 1200
+python linares/condicionalesdobles/condicionaldoble8.py "angie" 1200 1300 1400
+python linares/condicionalesdobles/condicionaldoble9.py "pedro" 50 40 60
+python linares/condicionalesdobles/condicionaldoble9.py "manuel" 75 30 20
+python linares/condicionalesdobles/condicionaldoble9.py "myrella" 55 58 65
+python linares/condicionalesdobles/condicionaldoble10.py "perla" 15 12 13
+python linares/condicionalesdobles/condicionaldoble10.py "Linda" 7 9 10
+python linares/condicionalesdobles/condicionaldoble10.py "pierina" 12 17 20
+python linares/condicionalesmultiples/condicionalmultiple1.py 3 6 2
+python linares/condicionalesmultiples/condicionalmultiple1.py 5 1 4
+python linares/condicionalesmultiples/condicionalmultiple1.py 2 6 1
+python linares/condicionalesmultiples/condicionalmultiple2.py 40 45 30
+python linares/condicionalesmultiples/condicionalmultiple2.py 50 20 15
+python linares/condicionalesmultiples/condicionalmultiple2.py 25 25 10
+python linares/condicionalesmultiples/condicionalmultiple3.py "taylor" 60 45 55
+python linares/condicionalesmultiples/condicionalmultiple3.py "miley" 45 45 55
+python linares/condicionalesmultiples/condicionalmultiple3.py "yudith" 30 70 25
+python linares/condicionalesmultiples/condicionalmultiple4.py "fiorella" 250 130 100
+python linares/condicionalesmultiples/condicionalmultiple4.py "anita" 150 150 190
+python linares/condicionalesmultiples/condicionalmultiple4.py "anderson" 120 180 185
+python linares/condicionalesmultiples/condicionalmultiple5.py "marta" 25 35 10
+python linares/condicionalesmultiples/condicionalmultiple5.py "keila" 20 15 10
+python linares/condicionalesmultiples/condicionalmultiple5.py "cristina" 21 14 4
+python linares/condicionalesmultiples/condicionalmultiple6.py "jesus" 18 19 18
+python linares/condicionalesmultiples/condicionalmultiple6.py "andrea" 17 18 17
+python linares/condicionalesmultiples/condicionalmultiple6.py "pedro" 15 14 17
+python linares/condicionalesmultiples/condicionalmultiple7.py "yasseni" 800 700 600
+python linares/condicionalesmultiples/condicionalmultiple7.py "jennie" 500 750 635
+python linares/condicionalesmultiples/condicionalmultiple7.py "luis" 450 350 700
+python linares/condicionalesmultiples/condicionalmultiple8.py "lissa" 1000 2350 2000
+python linares/condicionalesmultiples/condicionalmultiple8.py "jose" 1200 1300 2450
+python linares/condicionalesmultiples/condicionalmultiple8.py "eva" 1300 1250 2300
+python linares/condicionalesmultiples/condicionalmultiple9.py "julian" 45 42 46
+python linares/condicionalesmultiples/condicionalmultiple9.py "magaly" 44 44 35
+python linares/condicionalesmultiples/condicionalmultiple9.py "celeste" 40 45 20
+python linares/condicionalesmultiples/condicionalmultiple10.py "jisoo" 7 7 9
+python linares/condicionalesmultiples/condicionalmultiple10.py "fabricio" 6 9 4
+python linares/condicionalesmultiples/condicionalmultiple10.py "moises" 3 8 5
+python Rojas/simples/ejercicio01.py 10 5 60
+python Rojas/simples/ejercicio01.py 23 7 22
+python Rojas/simples/ejercicio01.py 0  9 78
+
+python Rojas/simples/ejercicio02.py 26 15 8
+python Rojas/simples/ejercicio02.py 7 8 11
+python Rojas/simples/ejercicio02.py 457 123 78
+
+python Rojas/simples/ejercicio03.py 70 1.68
+python Rojas/simples/ejercicio03.py 69 1.55
+python Rojas/simples/ejercicio03.py 99 1.83
+
+python Rojas/simples/ejercicio04.py 10 32 12
+python Rojas/simples/ejercicio04.py 55 100 21
+python Rojas/simples/ejercicio04.py 800 45 19
+
+python Rojas/simples/ejercicio05.py 10 9.8 60
+python Rojas/simples/ejercicio05.py 54 10  150
+python Rojas/simples/ejercicio05.py 7  9.8 44
+
+python Rojas/simples/ejercicio06.py 120 10 0.5
+python Rojas/simples/ejercicio06.py 50  10 3.2
+python Rojas/simples/ejercicio06.py 982  9.8 5.5
+
+python Rojas/simples/ejercicio07.py 1.2 0.004 105
+python Rojas/simples/ejercicio07.py 9.8 0.00016 144
+python Rojas/simples/ejercicio07.py 45  0.00035 59
+
+python Rojas/simples/ejercicio08.py 40 5 9
+python Rojas/simples/ejercicio08.py 148 3 27
+python Rojas/simples/ejercicio08.py 36 8 4
+
+python Rojas/simples/ejercicio09.py 10 5.5 7
+python Rojas/simples/ejercicio09.py 24 11 16
+python Rojas/simples/ejercicio09.py 42 21 23
+
+python Rojas/simples/ejercicio10.py 1 54 12
+python Rojas/simples/ejercicio10.py 0 100 99
+python Rojas/simples/ejercicio10.py 5 40 8
+
+python Rojas/dobles/ejercicio01.py 10 5 60
+python Rojas/dobles/ejercicio01.py 20 4 8
+python Rojas/dobles/ejercicio01.py 50 6 10
+
+python Rojas/dobles/ejercicio02.py 12 13 6
+python Rojas/dobles/ejercicio02.py 21 14 7
+python Rojas/dobles/ejercicio02.py 18 13 6
+
+python Rojas/dobles/ejercicio03.py 70 1.55
+python Rojas/dobles/ejercicio03.py 60 1.71
+python Rojas/dobles/ejercicio03.py 90 1.68
+
+python Rojas/dobles/ejercicio04.py 50 20 10
+python Rojas/dobles/ejercicio04.py 100 55 20
+python Rojas/dobles/ejercicio04.py 400 10 6
+
+python Rojas/dobles/ejercicio05.py 7  9.8 44
+python Rojas/dobles/ejercicio05.py 64 10  137
+python Rojas/dobles/ejercicio05.py 56 10  60
+
+python Rojas/dobles/ejercicio06.py 40 10 5
+python Rojas/dobles/ejercicio06.py 16 9.7 4
+python Rojas/dobles/ejercicio06.py 13 9.8 6
+
+python Rojas/dobles/ejercicio07.py 5.2 0.004 123
+python Rojas/dobles/ejercicio07.py 6.7 0.00026 50
+python Rojas/dobles/ejercicio07.py 84 0.00015  46
+
+python Rojas/dobles/ejercicio08.py 45 5 20
+python Rojas/dobles/ejercicio08.py 700 2  31
+python Rojas/dobles/ejercicio08.py 54 7 32
+
+python Rojas/dobles/ejercicio09.py 21 10 1
+python Rojas/dobles/ejercicio09.py 31 2 2
+python Rojas/dobles/ejercicio09.py 40 25 29
+
+python Rojas/dobles/ejercicio10.py 1 50 10
+python Rojas/dobles/ejercicio10.py 5 32 4
+python Rojas/dobles/ejercicio10.py 8 100 20
+
+python Rojas/multiples/ejercicio01.py 10 12 12
+python Rojas/multiples/ejercicio01.py 100 25 5
+python Rojas/multiples/ejercicio01.py 12 9 8
+
+python Rojas/multiples/ejercicio02.py 10 20 10
+python Rojas/multiples/ejercicio02.py 5 7 12
+python Rojas/multiples/ejercicio02.py 20 25 39
+
+python Rojas/multiples/ejercicio03.py 71 1.59
+python Rojas/multiples/ejercicio03.py 80 1.85
+python Rojas/multiples/ejercicio03.py 44 1.77
+
+python Rojas/multiples/ejercicio04.py 500 40 20
+python Rojas/multiples/ejercicio04.py 100 20 13
+python Rojas/multiples/ejercicio04.py 10 5 10
+
+python Rojas/multiples/ejercicio05.py 24 10 37
+python Rojas/multiples/ejercicio05.py 9 10 60
+python Rojas/multiples/ejercicio05.py 100 9.8 45
+
+python Rojas/multiples/ejercicio06.py 40 10 5
+python Rojas/multiples/ejercicio06.py 11 10 3
+python Rojas/multiples/ejercicio06.py 10 9.8 1
+
+python Rojas/multiples/ejercicio07.py 75 0.0019  37
+python Rojas/multiples/ejercicio07.py 45 0.000008  20
+python Rojas/multiples/ejercicio07.py 12 0.0002 -45
+
+python Rojas/multiples/ejercicio08.py 50 5 32
+python Rojas/multiples/ejercicio08.py 26 3 10
+python Rojas/multiples/ejercicio08.py 35 7 15
+
+python Rojas/multiples/ejercicio09.py 155 142 200
+python Rojas/multiples/ejercicio09.py 21 10 5
+python Rojas/multiples/ejercicio09.py 5 20 2
+
+python Rojas/multiples/ejercicio10.py 12 51 0
+python Rojas/multiples/ejercicio10.py 15 53 14
+python Rojas/multiples/ejercicio10.py 1 100 50
